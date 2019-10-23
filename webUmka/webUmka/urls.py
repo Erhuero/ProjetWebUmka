7bc3cc10957a1efc99148c3dfb5e7eb91373f47c
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-10-23 22:14:26 +0200

    Ajoutez des fichiers projet.

---
+++ webUmka/webUmka/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for webUmka.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', webUmka.views.home, name='home'),
+    # url(r'^webUmka/', include('webUmka.webUmka.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
